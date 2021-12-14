43f60d0275e4d1b9c20edd4219f3b3353041b7fc
harris305 <harrisms248@gmail.com>
2021-12-14 00:25:56 -0600

    added a README outlining the goals of the project, and a script that completes these requirements

---
+++ number_guesser/script.js
@@ -0,0 +1,28 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+console.log(compareGuesses(6, 4, generateTarget()));
+// Write your code below:
+function generateTarget() {
+    return Math.random() * 10; 
+}
+
+function compareGuesses(human, computer, secret) {
+  // Retrieve distance between human and secret
+  const humanDist = Math.abs(human - secret);
+  // Also retrieve distance between computer and secret
+  const compDist = Math.abs(computer - secret);
+  // If the human guess is closer, then return true
+  // If the computer guess is closer, then return false
+  // If both are tied, then return true
+  if (humanDist < compDist) {
+    return true;
+  } else if (humanDist > compDist) {
+    return false;
+  } else if (humanDist === compDist) {
+    return true;
+  }
+}
+
+
